5d5ffb806a586341b5d0eea3c47a75c34573327b
AlanCongregado <alan.congregado@hotmail.com>
2023-04-16 18:26:09 -0300

    commit

---
+++ main.js
@@ -0,0 +1,73 @@
+class ProductManager {
+  #id = 0;
+  constructor() {
+    this.products = [];
+  }
+  getProducts() {
+    return this.products;
+  }
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const product = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+    //agrego el id
+    product.id = this.#getId();
+    // Validar que los parametros sean obligatorios
+
+    // Validar si el producto ya se encuentra en la lista
+    for (const product of this.products) {
+      if (product.code === code) {
+        console.log("El producto ya se encuentra agregado");
+        return;
+      }
+    }
+    this.products.push(product);
+
+    // Si el producto NO se encuentra en la lista de productos entonces lo agrego
+  }
+  // Metodo para generar ID
+  #getId() {
+    this.#id++;
+    return this.#id;
+  }
+  getProductById(idProduct) {
+    const productId = this.products.findIndex(
+      (product) => product.id === idProduct
+    );
+    if (productId === -1) {
+      console.log("Producto No encontrado");
+      return;
+    } else {
+      console.log(this.products[productId]);
+    }
+  }
+}
+
+// Pruebas
+
+const productManager = new ProductManager();
+
+productManager.addProduct(
+  "producto prueba",
+  "Este es un producto de prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+productManager.addProduct(
+  "otro producto de prueba",
+  "Producto de prueba 2",
+  400,
+  "Sin imagen",
+  "abc123",
+  20
+);
+productManager.addProduct("Producto", "description", 250, "Sin", "123abc", 50);
+console.log(productManager.getProducts());
+console.log(productManager.getProductById());
